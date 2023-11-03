8c21d3f49d215e132242cfb5d33cba12ee895820
Christopher Zorn <christopher.zorn@gmail.com>
2023-11-03 15:40:30 -0700

    build-notes

--- scripts/build_blog.sh
+++ scripts/build_blog.sh
@@ -10,7 +10,10 @@ TEAR_DOWN="${2}"
 set -euo pipefail
 
 
-
+#The reflex init command creates a new Reflex app in the current directory. If a rxconfig.py file already exists already, it will re-initialize the app with the latest template.
+# XXX - What minimum code do we need?
+# XXX - Can we use an outside template on init?
+# https://github.com/reflex-dev/reflex/blob/6e1bce341288758972229772e664622604b4dfd5/reflex/constants/base.py#L77
 "${SCRIPT_DIR}"/init.sh "${PROJECT_NAME}"
 
 
@@ -20,6 +23,12 @@ reflex run &
 echo "$!" > ./reflex.pid
 
 open "http://localhost:3000"
+
+# Generate or copy pages
+
+# XXX - when deploying how do we make things composable and still use k8s?
+# XXX - how do we make everything gitops too?
+# XXX - do we build docker images locally or everything remotely?
 
 if [ -n "${TEAR_DOWN}" ]
 then
