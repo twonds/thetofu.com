1bf8eaba7f571fec159ec6ed668348d7d0ab968e
Christopher Zorn <christopher.zorn@gmail.com>
2023-12-15 16:36:33 -0800

    webhook

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -82,9 +82,33 @@ kubectl patch deployment \
 # Open a port to the argo UI
 # kubectl -n argo port-forward deployment/argo-server 2746:2746
 
-kubectl create rolebinding default-admin --clusterrole=admin --serviceaccount=argo:default --namespace=argo
+kubectl create rolebinding default-admin --clusterrole=admin --serviceaccount=argo:default --namespace=argo || echo
 
 # kubectl crossplane install provider crossplanecontrib/provider-kubernetes:v0.9.0
 kubectl apply -f argo/k8s-provider.yaml
 
 # What provider do we need?
+
+argo list -n argo
+
+# submit an example workflow
+argo submit -n argo --watch https://raw.githubusercontent.com/argoproj/argo-workflows/main/examples/hello-world.yaml
+
+
+argo list -n argo
+
+# setup github triggers
+# Make sure to have the eventbus deployed in the namespace.
+# We will use webhook event-source and sensor to trigger an Argo workflow.
+
+# Set up the operate-workflow-sa service account that the sensor will use
+# kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/master/examples/rbac/sensor-rbac.yaml
+
+kubectl apply -f argo/sensor-rbac.yaml
+
+# webhook event source
+# kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/stable/examples/event-sources/webhook.yaml
+
+kubectl apply -f argo/webhook.yaml
+
+
